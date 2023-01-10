193621e79417d8b078d5d7f190ad9c3d7bc208b1
Croidy <kert.roov@gmail.com>
2023-01-10 22:03:10 +0200

    Recipe screen started

--- App.py
+++ App.py
@@ -1,10 +1,12 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.lang import Builder
-from kivy.clock import Clock
+from kivy.properties import NumericProperty
+from kivy.lang import Builder       #Not Used
+from kivy.clock import Clock        #Not Used
 
-from kivy.uix.label import Label
+from kivy.uix.button import Button
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
 
 import pandas as pd
 import numpy as np
@@ -20,12 +22,17 @@ class RecipeScreen(Screen):
 
 
 class RecipeScrollView(ScrollView):
-    
-    def get_recipes(self):
-        view = self.parent.parent.ids['recipe_scrollview']
-        view.add_widget(Label(text='hello', font_size=50, size_hint_y=None, height=50))
+    recipes = os.listdir("data/recipes")
+    list_size = NumericProperty(len(recipes))
        
+class RecipeList(GridLayout):
+    recipes = os.listdir("data/recipes")
+    list_size = NumericProperty(len(recipes))
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        for recipe in RecipeScrollView.recipes:
+            self.add_widget(Button(text=recipe[:-4],size_hint_y=None,font_size=50))
 
 
 class MyScreenManager(ScreenManager):
@@ -33,7 +40,7 @@ class MyScreenManager(ScreenManager):
 
 
 class MoledoApp(App):
-    RecipeScreen = RecipeScreen()
+    RecipeScrollView = RecipeScrollView()
 
 
 
