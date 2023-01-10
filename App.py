7bd20bc0aafe17632d8fe2ff069b6d417c870026
Croidy <kert.roov@gmail.com>
2023-01-10 20:56:17 +0200

    Update data.

--- App.py
+++ App.py
@@ -1,21 +1,39 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
-import pandas as pd
+from kivy.lang import Builder
+from kivy.clock import Clock
 
+from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
+
+import pandas as pd
+import numpy as np
+import os
+#from functools import cache
 
 
 class MainScreen(Screen):
     pass
 
-class OtherScreen(Screen):
+class RecipeScreen(Screen):
     pass
+
+
+class RecipeScrollView(ScrollView):
+    
+    def get_recipes(self):
+        view = self.parent.parent.ids['recipe_scrollview']
+        view.add_widget(Label(text='hello', font_size=50, size_hint_y=None, height=50))
+       
+
+
 
 class MyScreenManager(ScreenManager):
     pass
 
 
 class MoledoApp(App):
-    pass
+    RecipeScreen = RecipeScreen()
 
 
 
